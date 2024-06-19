880e6c46b4d7e39d7a994b8791338daae5717554
pkathuri <pkathuri19@gmail.com>
2024-06-19 07:25:36 -0700

    first push

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print(f"Addition of 10 and 5 is {sum}")
+difference = number1 - number2
+print(f"Subtraction of 10 and 5 is {difference}")
+product = number1 * number2
+print(f"Multiplication of 10 and 5 is {product}")
