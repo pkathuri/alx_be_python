94a1b355aa792914599f793af9061c77726f6a04
pkathuri <pkathuri19@gmail.com>
2024-07-04 03:23:46 -0700

    sledge hammer

--- fns_and_dsa/arithimetic_operations.py
+++ fns_and_dsa/arithimetic_operations.py
@@ -1,18 +1,14 @@
-def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 != 0:
-                return num1 / num2
-            else:
-                return "Division by zero error"
-        case _:
-            return "Invalid operation"
-
-
-                
+def perform_operation(num1,num2,operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2!=0:
+            return num1 / num2
+        else:
+           return "Zero division error"
+    else:
+        return "invalid operation"   
