4045a73319ebc6b0cbbf93319d2f9e0c769495f0
pkathuri <pkathuri19@gmail.com>
2024-07-06 02:04:30 -0700

    commit changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(num1:float,num2:float,operation:str):
     match operation:
         case operation == "add":
             return num1 + num2
