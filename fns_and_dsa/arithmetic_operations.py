8a9ff6cea6d25d73d8459ddf5cf217f9746d39b9
pkathuri <pkathuri19@gmail.com>
2024-07-09 09:18:34 -0700

    final

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,15 @@
-def perform_operation(num1: float, num2: float, operation: str):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 != 0:
-                return num1 / num2
-            else:
-                return "zero division error"
-
-result = perform_operation(10.2, 5.1, "divide")
-print(result)
-
+def perform_operation(num1,num2,operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2!=0:
+            return num1 / num2
+        else:
+           return "Zero division error"
+    else:
+        return "invalid operation"
+            
