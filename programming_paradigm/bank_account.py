14de914f30adb4981a2856f8e0d8c06293a7cbec
pkathuri <pkathuri19@gmail.com>
2024-07-17 02:25:31 -0700

    correct version

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount():
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+    def deposit(self, amount):
+        self.account_balance += amount
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            # The statement measures if the withdrawal was a success
+            return True
+        else:
+            # This statement measures if the withdrawal was a fail.
+            return False
+    def display_balance(self):
+        print(f'The current account balance is {self.account_balance}')
+
+account = BankAccount(100)
+account.deposit(50)
+account.display()
