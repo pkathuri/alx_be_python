781b63b2d7a98ebdaad72073398904a0b891f118
pkathuri <pkathuri19@gmail.com>
2024-07-20 07:09:34 -0700

    final

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,18 +1,22 @@
 class BankAccount:
-    def __init__(self, account_balance = 0):
-        self.account_balance = account_balance
-
-    def deposit(self, amount):
-        self.account_balance += amount
-
-    def withdraw(self, amount):
+    def __init__(self,account_balance=0):
+        self.__account_balance = account_balance
+    def deposit(self,amount):
+        # In this case the return statement is absent because it is not important in deposit
+        # Encapsulation is employed in the account_balance attribute to make it private protecting data integrity
+       self.__account_balance +=  amount
+    def withdraw(self,amount):
         if amount <= self.account_balance:
-            self.account_balance -= amount
-            # The statement measures if the withdrawal was a success
+            self.__account_balance -= amount
             return True
         else:
-            # This statement measures if the withdrawal was a fail.
             return False
-
     def display_balance(self):
-        print(f'The current account balance is {self.account_balance}')
+        print(f'Your account balance is {self.__account_balance}')
+        
+# BankAccount(300)
+# BankAccount.deposit(200)
+my_account = BankAccount()
+my_account.deposit(200)
+my_account.display_balance()
+
