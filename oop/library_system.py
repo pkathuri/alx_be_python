86ed85b395b9755042e8c62242655bf11a53dbfa
pkathuri <pkathuri19@gmail.com>
2024-07-27 01:53:14 -0700

    initial commit

---
+++ oop/library_system.py
@@ -0,0 +1,36 @@
+
+# Parent class
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
+class EBook(Book):
+    def __init__(self,title,author,file_size):
+        # Instead of writing the self... words use the super method to inherit from the parent class
+        super().__init__(title,author)
+        self.file_size = file_size
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size {self.file_size}KB"
+
+class PrintBook(Book):
+    def __init__(self,title,author,page_count):
+        super().__init__(title,author)
+        self.page_count = page_count
+    def __str__(self):
+         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+        
+# The composition part of the program that inherits some attributes of the parent class
+# In composition the Library contains the other book classes both the EBook and PrintBook
+class Library:
+    def __init__(self):
+        self.books = []
+    def add_book(self,book):
+        self.books.append(book)
+    def list_books(self):
+        for book in self.books:
+            print(book)
+    
+
