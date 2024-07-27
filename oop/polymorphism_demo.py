7cee0f35e9171f02628154f919fa85531b94a3b7
pkathuri <pkathuri19@gmail.com>
2024-07-27 02:03:25 -0700

    initial commit

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,17 @@
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError("Suubclasses can only impement area method.")
+
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+     
+def Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * (self.radius**2)
