37b1999ff9e176c8f2aeb96f6d085714ca652fcf
pkathuri <pkathuri19@gmail.com>
2024-07-27 02:13:01 -0700

    final

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,12 +1,13 @@
-# Class and Static methods in action
 class Calculator:
     calculation_type = "Arithmetic Operations"
-    # The static method in action
+
     @staticmethod
-    def add(a,b):
-        return a+b
+    def add(a, b):
+        """Static method to add two numbers"""
+        return a + b
+
     @classmethod
-    def multiply(cls,a,b):
-         print(f"Calculation type: {cls.calculation_type}")
-         return a * b
-         
+    def multiply(cls, a, b):
+        """Class method to multiply two numbers"""
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b 
